fc82d2b93a45db1fc972207f86c1df4e55e56b32
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-11-17 12:43:58 -0500

    Initial commit

---
+++ Assets/_Scripts/LOS.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class LOS : MonoBehaviour
+{
+    public Collider2D collidesWith;
+    public ContactFilter2D contactFilter;
+    public List<Collider2D> colliders;
+
+    private BoxCollider2D LOSCollider;
+
+
+    private void Start()
+    {
+        LOSCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Physics2D.GetContacts(LOSCollider, contactFilter, colliders);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        collidesWith = other;
+
+
+    }
+}
